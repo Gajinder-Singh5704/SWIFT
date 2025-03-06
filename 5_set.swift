6308a7d964076ca5225ee9e5282db847960684a1
Gajinder-Singh5704 <thakur.somu5704@gmail.com>
2025-03-06 18:14:56 +0530

    set

---
+++ 5_set.swift
@@ -0,0 +1,25 @@
+//SET
+var interns = Set(["GAJINDER", "DIVAY", "SOM", "VAANI","ABHI","AJAY","AJAY","SOM","VAANI"])
+
+print(interns)
+print()
+
+//EMPTY SET 
+let empty_set = Set<String>()
+print(empty_set)
+print()
+
+//SET METHODS
+print(interns.sorted())     //.sorted()
+print()
+
+interns.insert("KARTIK") //.insert()
+print(interns)
+print()
+
+print(interns.contains("SOM"))      //.contains()
+print(interns.contains("OM"))
+print()
+
+print(interns.count)        //.count
+print()
