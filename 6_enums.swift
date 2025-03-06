f40113eea603037b3d40fc04161b39addd8e11a5
Gajinder-Singh5704 <thakur.somu5704@gmail.com>
2025-03-06 18:15:14 +0530

    enums

---
+++ 6_enums.swift
@@ -0,0 +1,21 @@
+//ENUMS
+
+//ENUM DEFINITION
+enum interns {
+    case Gajinder
+    case Divay
+    case Vaani
+    case Ajay
+    case Rajat
+}
+
+print(interns.Gajinder)
+
+enum interns1 {
+    case Gajinder,
+     Divay,
+     Vaani,
+     Ajay,
+     Rajat
+}
+print(interns1.Divay)
