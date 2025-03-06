0a517efd3b94c0e8d792b2d40091b978229af2b8
Gajinder-Singh5704 <thakur.somu5704@gmail.com>
2025-03-06 16:45:34 +0530

    booleans and string interpolation

---
+++ 2_bool_interpolation.swift
@@ -0,0 +1,23 @@
+
+//BOOLEAN
+var b1 = true       //BOOLEAN TRUE
+var b2 = false      //BOOLEAN FALSE
+print(b1,b2)
+print()
+
+//TOGGLE
+print("B2 IS \(b2)")
+b2.toggle()
+print("BUT NOW B2 IS \(b2)")        //TOGGLE
+print()
+
+//STRING CONCATENATION
+print(str1 + " WORLD")      //USING '+'
+print()
+
+//STRING INTERPOLATION
+var str1 = "HELLO"
+print("\(str1) WORLD")      //USING '\()'
+var num = 1
+print(str1 + String(num))  //to concat numbers with string
+print()
