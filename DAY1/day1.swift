de7c160a21a3ced0c9da95c7cba0f2e4e0d245bf
GautamBhatt <bhattgautam575@gmail.com>
2025-03-06 16:05:58 +0530

    DAY 1 SWIFT

---
+++ DAY1/day1.swift
@@ -0,0 +1,40 @@
+// HELLO WORLD
+print("Hello World")
+print()
+
+// VAR
+var num = 10        //VARIABLE
+print(num)
+print()
+
+//  LET
+let num2 = 11;      //CONSTANT
+print(num2)
+print()
+
+//STRINGS
+var str = "HELLO"       //STRING
+str = "\"WORLD\""       //ESCAPE SEQUENCE
+print(str)
+str = """
+HELLO
+WORLD
+"""
+print(str)
+
+//STRING METHODS
+print("COUNT OF STR : \(str.count)")            //.count 
+print("LOWERED STR : \(str.lowercased())")      //to lower case 
+print("UPPERED STR : \(str.uppercased())")      //to upper case
+print("HAS PREFIX : \(str.hasPrefix("H"))")     //hasPrefix()
+print("HAS SUFFIX : \(str.hasSuffix("D"))")     //hasSuffix()
+
+print()
+
+//INTEGERS
+var num1 = 10       //INTEGER
+print(num1)     
+
+//DOUBLE
+var numm2 = 11.1  //DOUBLE
+print(numm2)
