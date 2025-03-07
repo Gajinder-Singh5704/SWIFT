0a12d1141a6f63741e033e132e89ed8002f62024
Gajinder-Singh5704 <thakur.somu5704@gmail.com>
2025-03-07 13:51:36 +0530

    OPERATORS

---
+++ 9_conditional_statements.swift
@@ -0,0 +1,40 @@
+// IF CONDITION
+var age = 17
+if age >= 18 {
+    print("YOU'RE ELIGIBLE TO VOTE")  
+}
+
+//COMPARE ENUMS
+enum temp : Comparable {
+    case low
+    case mid
+    case high
+}
+
+let temp_2 = temp.low
+let temp_1 = temp.high
+
+print(temp_1 > temp_2)  //The output will be true because low comes before high in the enum list.
+
+//IF-ELSE CONDITIONS
+if age >= 18 {
+    print("YOU'RE ELIGIBLE TO VOTE")  
+}
+else{
+    print("YOU'RE NOT ELIGIBLE TO VOTE")
+} 
+
+//SWITCH CASE
+var tmp = "high"
+
+switch (tmp) {
+case "high":
+    print("TEMPERAURE IS HIGH");
+case "mid":
+    print("TEMPERAURE IS MID");  
+case "low":
+    print("TEMPERAURE IS LOW");  
+    
+default:
+     print("NONE"); 
+}
