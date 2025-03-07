8dda1031a7fe261daee063cc5ab68a08aa5ba82d
Gajinder-Singh5704 <thakur.somu5704@gmail.com>
2025-03-07 13:52:20 +0530

    LOOPS

---
+++ 10_loops.swift
@@ -0,0 +1,17 @@
+//FOR LOOP
+let interns = ["GAJINDER","DIVAY","VAANI","AJAY"]
+for intern in interns{
+    print(intern);
+}
+
+// FOR IN RANGE
+for i in 1...10 {
+    print("2 X \(i) = \(2*i)")
+}
+
+//WHILE LOOP
+var i = 1;
+while(i<11){
+    print("3 X \(i) = \(3*i)")
+    i += 1
+}
