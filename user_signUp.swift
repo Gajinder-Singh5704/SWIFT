c8f377aadc3559979cbc025dda23dfa7af714e62
Gajinder-Singh5704 <thakur.somu5704@gmail.com>
2025-03-12 18:13:40 +0530

    USER SIGNUP

--- user_signUp.swift
+++ user_signUp.swift
@@ -1,7 +1,3 @@
-//USER-SIGNUP PROGRAM
-//ADDS A NEW USER
-//CHECKS FOR EXISTING USER
-
 var users : [User] = []
 
 struct User{
@@ -11,25 +7,22 @@ struct User{
     var password : String
 }
 
-var username : String = ""
-var email : String = ""
-var password : String = ""
-
 var user1 = User(id:1,username:"Gajinder",email:"thakur.somu1998@gmail.com",password:"abc123")
 users.append(user1)
 
 user1 = User(id:2,username:"Som",email:"thakur.somu5704@gmail.com",password:"abc123")
 users.append(user1)
 
-func userInput() -> Bool {
+func userInput() -> (String, String, String) {
     
     print("Enter username : ",terminator : "")
-    username = readLine() ?? ""
+    let username = readLine() ?? ""
     print("Enter your email : ", terminator: "")
-    email = readLine() ?? ""
+    let email = readLine() ?? ""
     print("Enter passowrd : ", terminator: "")
-    password = readLine() ?? ""
-    return true
+    let password = readLine() ?? ""
+    
+    return (username,email,password)
 }
 
 func userExists(email : String) -> Bool {
@@ -41,23 +34,23 @@ func userExists(email : String) -> Bool {
 
 let addUser : (String ,String ,String) -> Bool = {
     (username,email,password) in 
-    let user = User(id:1,username : username,email : email,password : password)
+    let user = User(id: users.count + 1, username: username, email: email, password: password)
     users.append(user)
     // print(user)
     return true
 }
 
-func main() -> (){
-    if(userInput())
-    {
+func main() {
+    let (username,email,password) = userInput()
+
         if(!userExists(email : email)){
-            if (addUser("ok","ok","ok")){
+            if addUser(username,email,password){
                 print("User added Successfully")
+                print("UPDATED USERS LIST : \(users)")
             }
         }
         else{
             print("USER ALREADY EXISTS")
         }
     }
-}
 main()
